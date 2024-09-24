a6cd2d7a418647e87f0ee93e8197b6aa775d3b55
jucastro15 <ra56421610838@acaonsfatima.org.br>
2024-09-23 22:43:46 -0300

    ops

--- src/pages/principal/index.jsx
+++ src/pages/principal/index.jsx
@@ -11,65 +11,65 @@ export default function Principal() {
     return (
         <div className='app-container'>
             <Cab />
- <section className='section-cards'>
-       <h1>Conteúdos</h1>
+            <section className='section-cards'>
+                <h1>Conteúdos</h1>
 
-            <section className='cards-container'>
+                <section className='cards-container'>
 
-             
 
-                <Cards
-                    corPrimaria={corPrimaria}
-                    corSegundaria={corSegundaria}
-                    titulo="Componentes"
-                    topicos="
+
+                    <Cards
+                        corPrimaria={corPrimaria}
+                        corSegundaria={corSegundaria}
+                        titulo="Componentes"
+                        topicos="
                     - Objetos e Listas de Objetos
                     - Componentes"
-                    date="26/agosto"
-                    mostrarTrabalho={false}
-                />
+                        date="26/agosto"
+                        mostrarTrabalho={false}
+                    />
 
 
-                <Cards
-                    corPrimaria="#DCA3E5"
-                    corSegundaria="#EDE2FB"
-                    titulo="Renderização e Efeito"
-                    topicos="
+                    <Cards
+                        corPrimaria="#DCA3E5"
+                        corSegundaria="#EDE2FB"
+                        titulo="Renderização e Efeito"
+                        topicos="
                     - Componentes na renderização."
-                    date="02/setembro"
-                    mostrarTrabalho={false}
-                />
+                        date="02/setembro"
+                        mostrarTrabalho={false}
+                    />
 
 
-                <Cards
-                    corPrimaria="#C3C3C3"
-                    corSegundaria="#F2F2F2"
-                    titulo="Consumindo APIs - Parte 1"
-                    topicos="
+                    <Cards
+                        corPrimaria="#C3C3C3"
+                        corSegundaria="#F2F2F2"
+                        titulo="Consumindo APIs - Parte 1"
+                        topicos="
                     - Componentes na renderização."
-                    date="09/setembro"
-                    mostrarTrabalho={true}
-                />
-                <Cards
-                    corPrimaria="#F6E448"
-                    corSegundaria="#FAF4C1"
-                    titulo="Prova Prática e Teorica"
-                    topicos="
+                        date="09/setembro"
+                        mostrarTrabalho={true}
+                    />
+                    <Cards
+                        corPrimaria="#F6E448"
+                        corSegundaria="#FAF4C1"
+                        titulo="Prova Prática e Teorica"
+                        topicos="
                     - Criando um projeto React
                     - Estilização com SCSS
                     - Navegação entre telas
                     - Variáveis de estado
                     - Renderização de lista
                     - Componentes"
-                    date="16/setembro"
-                    mostrarTrabalho={false}
-                />
+                        date="16/setembro"
+                        mostrarTrabalho={false}
+                    />
 
 
 
 
+                </section>
             </section>
-</section>
         </div>
     )
 
